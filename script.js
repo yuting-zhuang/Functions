66c028600daaff7ccefee61168166b1ebb519048
yuting <zhuay220@newschool.edu>
2025-03-28 21:04:08 -0400

    added words to each categories

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
-let myNaturalList = [ {“subjects” : ["word1", "word2", "jj"],
-	"nouns" : ["blablabla", "blablba"]
-}]
-let mySurrealistList = ["jdfkjsdf", ""]
+//Define an object that contains the vocabulary lists for all styles.
+const wordBank = {
+	surreal:{
+		subjects:[
+			"The dream machine", "The bionic sheep","The lunar fish"
+		],
+
+		verbs:[
+			"scans", "dissolves into", "echoes through"
+		],
+
+		adjectives:[
+			"pixelated", "invisible", "glitching"
+		],
+
+		objects:[
+			"time loops", "a floating city", "memory fragments"
+		]
+	}
+
+	nature:{
+		subjects:[
+			"The mossy stone", "The glowing firefly","The river"
+		],
+
+		verbs:[
+			"sings to", "flows over", "wraps"
+		],
+
+		adjectives:[
+			"shimmering", "soft-spoken", "misty"
+		],
+
+		objects:[
+			"heartbeat of earth", "fading sun", "dewdrop stories"
+		]
+	}
+};
+
